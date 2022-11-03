4fe74768179104a670c01b7f4fbbf0f60b666a64
Rutuja R Mashere <96306705+RutujaRM@users.noreply.github.com>
2022-11-03 16:41:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Text  from "./Component/Text";
+
+export default function App() {
+  return (
+    <div className="App">
+     <Text/>
+    </div>
+  );
+}
---
+++ src/Component/Text.js
@@ -0,0 +1,22 @@
+const Text = (_) => {
+  return (
+    <>
+      <div>
+        <h1 className="margin-top">The Evolution of</h1>
+        <br />
+        <h1>Airbnb's Frontend </h1> <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>@spikebrehm </h2>
+        <img
+          src="https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/815/cached.offlinehbpl.hbpl.co.uk/news/ORP/Airbnbnewbrand-20140718083906597.jpg"
+          alt="not display"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Text;
